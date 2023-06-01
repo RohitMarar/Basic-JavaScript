edecfbb9ab1ba495c63d80d50b78607d3a28711f
Rohit Marar <rohitmarar108@gmail.com>
2023-06-01 16:37:25 +0545

    Loop array object

--- index.js
+++ index.js
@@ -1,6 +1,339 @@
-const length=prompt("Hawa")
-const le=prompt("Haw")
-const area=length*le
-console.log(area)
-alert("The area is  : "+area)
-document.write("The area is : " +area)
+// const length=prompt("Hawa")
+// const le=prompt("Haw")
+// const area=length*le
+// console.log(area)
+// alert("The area is  : "+area)
+// document.write("The area is : " +area)
+
+//Variable
+// var x = 69
+// console.log(x)
+
+// x = 420
+// console.log(x)
+
+// var x=6+9
+// console.log(x)
+
+// let y = 69;
+// console.log(y)
+//  y = 420
+// console.log(y)
+
+// y = 3 + 3
+// console.log(y)
+
+// const z = 69
+// console.log(z)
+
+// z = 420
+// console.log(z)
+
+// const z = 36
+// console.log(z)
+
+// const name = "Rohit Marar"
+// const y = 69
+// const ans = name+ y
+// console.log(name )
+// console.log(ans)
+
+// let y = true 
+// console.log(typeof y)
+ 
+// let x = 69
+// let y = 420
+// let z = true
+// let ans= x+y+z
+// console.log(ans)
+
+// let y=null
+// console.log(typeof y)
+
+// let z
+// console.log(typeof z)
+
+// const name = "Rohit"
+// const lname = "Marar"
+// console.log(`Haina K ho yesto : ${name} ${lname}`)
+
+// const name = "Rohit"
+// const lname="Marar"
+// const age=42
+// const address="Pepsicola"
+// console.log(`My name is ${name} ${lname}. My age is ${age}. I live in ${address}.`)
+
+//type conversion
+// const x=69
+// console.log(x)
+// console.log(typeof x)
+// console.log(typeof String(x))
+// const x="Rohit "
+// console.log(x)
+// console.log(typeof x)
+// console.log(typeof Number(x))
+
+//Strings Method
+//let result="Hello World"
+// //console.log(result[0])
+// console.log(result.length)
+
+//concatination
+//console.log(result.concat(" LOLOLOLOL"))
+
+//replace
+//console.log(result.replace("HEHEHH","NOICE"))
+
+//split
+//console.log(result.split(" "))
+
+//charAt
+//console.log(result.charAt(5))
+
+//indexof
+//console.log(result.indexOf("H"))
+
+//inculdes
+//console.log(result.includes("EHEHE"))
+
+//trim
+//console.log(result.trim()) //removes all unwanted spaces by the user 
+
+//substring
+//console.log(result.substring(0,4))
+
+//slice
+//console.log(result.slice(0,4))
+
+// console.log(result.toLowerCase())
+// console.log(result.toUpperCase())
+
+//if else
+
+// let email="rohitmarar108@gmail.com"
+// let password="696969696969"
+// if (email === "rohitmarar108@gmail.com" && password === "6969696969"){
+//     console.log("Welcome!!!")
+// }else{
+//     console.log("NOT WELCOME!!!")
+// }
+
+// let day = "Sunn"
+// if(day === "Rainy"){
+//     console.log("It is a rainy day!")
+// }else if(day === "Cloudy"){
+//     console.log("It is a gloomy day!")
+// }else if(day === "Sunny"){
+//     console.log("Last Garmi!!!")
+// }else if(day === "winter"){
+//     console.log("tyo samaya pani aaudaixa!")
+// }else{
+//     console.log("Invalia day!!!")
+// }
+
+// let x = 10
+// if(x>15){
+//     console.log("x is greater than 15")
+// }else if(x>5){
+//     console.log("x is greater than 5, but not greater than 15")
+// }else{
+//     console.log("x is not greater than 15")
+// }
+
+// let grade="f"
+// if(grade==="A")
+// {
+//     console.log("Excellent!!!")
+// }
+// else if(grade==="B")
+// {
+//     console.log("Very good!")
+// }
+// else if(grade==="C")
+// {
+//     console.log("Good!")
+// }
+// else if(grade==="D")
+// {
+//     console.log("BAD!!!")
+// }
+// else
+// {
+//     console.log("Invalid Grade")
+// }
+
+//Switch
+// let day=new Date().getDay()
+// switch (day) {
+//     case 0:
+//         console.log("Today is Sunday!!!")
+        
+//        // break;
+//     case 1:
+//         console.log("Today is Monday!!!")
+//        // break
+//     case 2:
+//         console.log("Today is Tuesday!!!")
+        
+//         //break;
+//     case 3:
+//         console.log("Today is Wednesday!!!")
+            
+//         //break;
+//     case 4:
+//         console.log("Today is Thursday!!!")
+            
+//         //break;
+//     case 5:
+//         console.log("Today is Friday!!!")
+            
+//         // break;
+
+//     default:
+//         console.log("Today is Saturday!!!")
+//         //break;
+// }
+
+// let month="June"
+// switch (month) {
+//     case "January":
+//     case "February":
+//     case "March":
+//         console.log("We are in Q1!!!")
+        
+//         break;
+//     case "April":
+//     case "May":
+//     case "June":
+//         console.log("We are in Q2!!!")
+//         break
+//     case "July":
+//     case "August":
+//     case "September":
+//         console.log("We are in Q3!!!")
+//         break
+
+//     default:
+//         console.log("We are in Q4!!!")
+//         break;
+//}
+
+//let grade="D"
+// switch (grade) {
+//     case "A":
+//         console.log("Excellent!!!")
+        
+//         break;
+//     case "B":
+//         console.log("Very Good")
+            
+//             break;
+//     case "C":
+//         console.log("Good!!!")
+                
+//                 break;
+//     case "D":
+//         console.log("FAIL!!!")
+                    
+//                     break;
+//     default:
+//         console.log("Invalid Grade!!!")
+//         break;
+// }
+
+//LOOP
+
+// for(let i = 0; i < 10; i++){
+//     console.log(Math.random())
+// }
+
+// for(let i=0;i<=10;i++){
+//     if(i%2!==0){
+//         console.log(i)
+//     }
+    
+// }
+
+// let input = prompt("Enter a number")
+// let num= Number(input)
+// if(isNaN(num)){
+//     alert("Enter a valid number!!!")
+// }else{
+//     document.write("Even numbers are:")
+//      for(let i=0;i<=10;i++){
+//         if(i%2 ===0){
+//             document.write("<br>",i)
+//         }
+//      }
+// }
+
+// let ip = prompt("Enter a number")
+// let num=Number(ip)
+// if(isNaN(num)){
+//     alert("Enter a valid number!!!")
+// }
+//     else{
+//         document.write("Odd Numbers Are:")
+//         for(let i=0;i<=1000;i++){
+//             if(i%2!==0){
+//                 document.write("<br>",i)
+//             }
+//         }
+//     }
+
+//while loop
+
+// let i = 0
+// while (i<= 10) {
+//     console.log("Iteration",i)
+//     i++
+// }
+
+//do while
+// let i=0
+// do {
+//     console.log("HEHEHEHE")
+//     i++
+// } while (i<10);
+
+//array
+
+// const arr = [6,9,4,2,"Rohit",0]
+
+// console.log(arr[4])
+
+// const arr=[]
+// arr[0]=69
+// arr[1]=420
+// arr[2]="HEHEHE"
+// console.log(arr)
+ 
+//const arr=[6,9,4,2,0]
+
+//console.log(arr.length)
+
+//push
+
+//arr.push(420) // add in last
+//arr.pop()
+//arr.unshift(1) // add in the  first index
+//arr.shift() // remove in first 
+// const result = arr.includes(8) //finds and returns the num in array
+// console.log(arr)
+// console.log(result)
+
+// const ans = arr.splice(2,2) // removes 2 itmes after index 2 including index 2 item
+// console.log(arr)
+// console.log(ans)
+
+// const ans = arr.slice(1,2) // 
+// console.log(arr)
+// console.log(ans)
+
+//object
+const obj = {
+    name : "Broky",
+    age : 20,
+    country : "Latvia",
+}
+console.log(obj)
